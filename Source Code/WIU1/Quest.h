6281c149102faa0a013173b5682c4aa960657ce8
Muhammad Irfan Bin Safaruddin <254503X@mymail.nyp.edu.sg>
2026-08-18 17:14:28 +0800

    Update Quest.h

--- Source Code/WIU1/Quest.h
+++ Source Code/WIU1/Quest.h
@@ -1,6 +1,20 @@
 #pragma once
+#include "GameObject.h"
+#include <string>
 
-class Quest
+class Quest  
 {
+private:
+    bool Completed;   
+    std::string questName;
+    std::string questDescription;
+
+public:
+    void completeQuest();
+    bool isCompleted() const;
+    Quest(const std::string& name, const std::string& description);
+
+    std::string getName() const;
+    std::string getDescription() const; 
 
 };
