31cb0bfa264301fd49bb8d3c0866cf6e659bb2ba
Muhammad Irfan Bin Safaruddin <254503X@mymail.nyp.edu.sg>
2026-08-19 13:57:51 +0800

    NPC Updated

--- Source Code/WIU1/NPC.cpp
+++ Source Code/WIU1/NPC.cpp
@@ -1,1 +1,22 @@
 #include "NPC.h"
+#include <iostream>
+ 
+void NPC::talk() {
+    std::cout << name << ": " << dialogue << std::endl;   
+    if (quest != nullptr) {
+        if (!quest ->isCompleted()) {
+            std::cout << "Quest: " << quest->getName() << std::endl;
+            std::cout << quest->getDescription() << std::endl;
+        }
+        else {
+            std::cout << "Quest Completed!" << std::endl;
+        }
+    }
+}
+
+Quest* NPC::getQuest() const {
+    return quest;
+}
+NPC::NPC(const std::string& name, const std::string& description, int x, int y, char symbol, const std::string& dialogue, Quest* quest)
+    : GameObject(name, description, x, y, symbol), dialogue(dialogue), quest(quest) {
+}
--- Source Code/WIU1/NPC.h
+++ Source Code/WIU1/NPC.h
@@ -1,7 +1,19 @@
 #pragma once
-#include "Entity.h"
+#include "GameObject.h"
+#include "Quest.h"
+#include <string>
+
 class NPC :
-    public Entity
+    public GameObject
 {
+private:
+    std::string dialogue;
+    Quest* quest;  
+ 
+public:
+    NPC(const std::string& name, const std::string& description, int x, int y, char symbol, const std::string& dialogue, Quest* quest);
+    void talk();
+    Quest* getQuest() const;
+
 };
 
