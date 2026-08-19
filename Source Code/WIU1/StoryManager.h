5752dd0e0b5d648c79104cd38a820ab8d1ae8b30
Muhammad Irfan Bin Safaruddin <254503X@mymail.nyp.edu.sg>
2026-08-19 15:40:13 +0800

    Update StoryManager.h

--- Source Code/WIU1/StoryManager.h
+++ Source Code/WIU1/StoryManager.h
@@ -1,15 +1,45 @@
 #pragma once
+#include "Quest.h"
+#include "NPC.h"
 
 class StoryManager
 {
 private:
+    Quest killZombieQuest;
+    Quest findMissingPersonQuest;
+    Quest findPharmacyQuest;
+    
+    NPC zombieNPC;
+    NPC missingpersonNPC;
+    NPC pharmacyNPC;
+
+
 	int TimeToNextEvent = 0;
 	int MessageRand = 0;
+<<<<<<< Updated upstream
 	int DoneSideQuests = 0;
+=======
+
+>>>>>>> Stashed changes
 protected:
 	int EventTimer = 0;
 	int SideQuest = 0;
+    
 public:
+    StoryManager();
+
+    NPC& getZombieNPC();
+    NPC& getmissingpersonNPC();
+    NPC& getpharmacyNPC();
+
+    Quest& getkillZombieQuest();
+    Quest& getfindMissingPersonQuest();
+    Quest& getfindPharmacyQuest();
+
+    bool allQuestsCompleted() const;
+
+    int getCompletedQuestsCount() const;
+
 	int GetTimeToNextEvent() const;
 	int GetMessageRand() const;
 	void SetTimeToNextEvent();
