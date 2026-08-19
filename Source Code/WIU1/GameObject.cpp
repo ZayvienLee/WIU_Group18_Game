f803a9d6498cea862e3087d38eebd2495b293ebb
Muhammad Irfan Bin Safaruddin <254503X@mymail.nyp.edu.sg>
2026-08-19 14:11:23 +0800

    Update GameObject.cpp

--- Source Code/WIU1/GameObject.cpp
+++ Source Code/WIU1/GameObject.cpp
@@ -1,2 +1,7 @@
 #include "GameObject.h"
 
+GameObject::GameObject(const std::string& name, const std::string& description, int x, int y, char symbol)
+    : name(name), description(description), positionX(x), positionY(y), symbol(symbol) {
+        
+    }
+
