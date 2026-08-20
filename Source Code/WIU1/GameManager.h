50f2b4b3cca1d3ee4ed9f0aa3341b18e86eb36df
ZayvienLee <zayvienlee@gmail.com>
2026-08-20 15:07:23 +0800

    Player and GameManager Change

--- Source Code/WIU1/GameManager.h
+++ Source Code/WIU1/GameManager.h
@@ -25,7 +25,7 @@ public:
 	void exitBuilding();
 
 	/* To handle Item management */
-	void handleItemPickup(Player& player, Map& currentMap);
-	void handleItemDrop(Player& player, Map& currentMap, int slotNumber);
-	void rewardPlayerFromNPC(Player& player, Map& currentMap, Item* questReward);
+	void handleItemPickup(Player& player, Map& currentMap) const;
+	void handleItemDrop(Player& player, Map& currentMap, int slotNumber) const;
+	void rewardPlayerFromNPC(Player& player, Map& currentMap, Item* questReward) const;
 };
