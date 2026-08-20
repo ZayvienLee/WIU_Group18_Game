5aacc4b1c9bbf2287a20d3b6e53699ff0d029183
Muhammad Irfan Bin Safaruddin <254503X@mymail.nyp.edu.sg>
2026-08-20 10:22:25 +0800

    Update Item.h

--- Source Code/WIU1/Item.h
+++ Source Code/WIU1/Item.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "GameObject.h"
+#include <string>
 
 class Item : public GameObject
 {
+public:
+	Item(std::string name, std::string description, int x, int y, char symbol);
 	
 };
