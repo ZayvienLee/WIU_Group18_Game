1b8bfb9d1a72884d84414eca1fb8679ce265b22c
Lexi-03 <maverickyeozh@gmail.com>
2026-08-21 10:50:09 +0800

    Update Weapon.cpp

--- Source Code/WIU1/Weapon.cpp
+++ Source Code/WIU1/Weapon.cpp
@@ -1,7 +1,17 @@
 #include <string>
 #include "Weapon.h"
 
-Weapon::Weapon(std::string n, std::string desc, char sym, int atk, int atkRange
+// Default values will be assigned to unknown weapon
+Weapon::Weapon()
+{
+	name = "Unknown Weapon Name";
+	description = "This weapon has an unknown description";
+	symbol = '?';
+	atkRange = 0;
+	wDamage = 0;
+}
+
+Weapon::Weapon(std::string n, std::string desc, char sym, int dmg, int atkR)
 {
 	// GameObject var
 
@@ -9,6 +19,18 @@ Weapon::Weapon(std::string n, std::string desc, char sym, int atk, int atkRange
 	description = desc;
 	symbol = sym;
 
-	atkRange = atkRange;
-	dmg = atk;
+	// Weapon var
+
+	atkRange = atkR;
+	wDamage = dmg;
+}
+
+int Weapon::getAtkRange() const
+{
+	return atkRange;
+}
+
+int Weapon::getDamage() const
+{
+	return wDamage;
 }
