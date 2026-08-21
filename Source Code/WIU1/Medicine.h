35bd94e05bcb4366b73b7b17fe6a11eab28a6c33
W-haobing <judywang0720@gmail.com>
2026-08-21 10:40:18 +0800

    Update Medicine.h/.cpp

--- Source Code/WIU1/Medicine.h
+++ Source Code/WIU1/Medicine.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "Item.h"
+#include "Player.h"
 
 class Medicine : public Item
 {
+private:
+	int recoverHp;
 
+public:
+	Medicine();
+
+	void consume(Player& player) override;
 };
